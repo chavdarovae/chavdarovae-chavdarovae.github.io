5ea27460a10931109402cbcc31faef1c1b77303a
chavdarovae <chavdarova.e@gmail.com>
2020-10-25 08:16:57 +0200

    Expanding course cards

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const selectors = {
     main: '.main',
     navBtn: '.site-nav__list-link',
+    courseInstance: '.course',
+    curCourseInstance: '.course--current',
 }
 
 class HomePageRouter {
@@ -10,14 +12,26 @@ class HomePageRouter {
 
     registerEventHandlers() {
         this.handleNavBtnClick();
+        this.handleCourseInstanceClick();
     }
 
     handleNavBtnClick() {
-        const { navBtn, main } = selectors;
+        const { navBtn, main,curCourseInstance } = selectors;
         $(navBtn).on('click', (e) => {
             const mainComponentId = '#' + e.target.text.toLowerCase();
+            if(mainComponentId==='#courses') {
+                $(curCourseInstance).removeClass('course--current');
+            }
             $(main).children().filter(':visible').addClass('hidden');
             $(mainComponentId).removeClass('hidden');
+        })
+    }
+
+    handleCourseInstanceClick() {
+        const { courseInstance, curCourseInstance } = selectors;
+        $(courseInstance).on('click', (e) => {
+            $(curCourseInstance).removeClass('course--current');
+            $(e.target).parents(courseInstance).addClass('course--current');
         })
     }
 }
