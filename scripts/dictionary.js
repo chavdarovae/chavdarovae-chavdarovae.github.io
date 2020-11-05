e3a5f9e41072208566d450f5c8b97b913a732135
chavdarovae <chavdarova.e@gmail.com>
2020-11-05 19:17:55 +0100

    Try

--- scripts/dictionary.js
+++ scripts/dictionary.js
@@ -14,7 +14,7 @@ const $bodyContentDe = $(`
 <div class="content-container">
     <div class="header__logo header__logo--backend">
         <img src="./assets/icons/noun_back end_133744.svg" alt="back-end-icon">
-        <div class="header__logo-lang-bar header__logo-lang-bar--backend">DE</div>
+        <div class="header__logo-lang-bar header__logo-lang-bar--backend">FR</div>
     </div>
     <nav class="nav">
         <ul class="nav__list row row--right-aligned">
@@ -26,7 +26,7 @@ const $bodyContentDe = $(`
     </nav>
     <div class="header__logo header__logo--frontend">
         <img src="./assets/icons/noun_front end_133743.svg" alt="front-end-icon">
-        <div class="header__logo-lang-bar header__logo-lang-bar--frontend">DE</div>
+        <div class="header__logo-lang-bar header__logo-lang-bar--frontend">FR</div>
     </div>
 </div>
 </header>
